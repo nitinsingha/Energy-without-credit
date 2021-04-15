518f34c085e5988d97e820ec343a5203c7722e88
shreyasv11 <64632587+shreyasv11@users.noreply.github.com>
2021-04-15 15:56:17 +0530

    Add files via upload

---
+++ Double Auction/problemA.m
@@ -0,0 +1,28 @@
+function [C_new,D_new] = problemA(B,S,neta,rho,C_min,C_max,D_max)
+
+m = size(B,1);
+n = size(B,2);
+
+C = optimvar('x',m,n,'LowerBound',0);     % Electricity demand of charging vehicles
+D = optimvar('y',m,n,'LowerBound',0);      % Electricity supply by discharging vehicles
+
+prob = optimproblem;
+prob.Objective = fcn2optimexpr(@objective,C,D,B,S);    
+prob.Constraints.cons1 = (sum(C,2)*neta >= C_min);            % These are the constraints
+prob.Constraints.cons2 = (sum(C,2)*neta <= C_max);            % for the optimisation
+prob.Constraints.cons3 = (transpose(sum(D))<=D_max);          % problem. In the paper,
+prob.Constraints.cons4 = (rho*D == C);                        % they are written in eqn. (4).
+
+
+x0.x = zeros(m,n);
+x0.y = zeros(m,n);
+
+options = optimoptions(@fmincon,'MaxFunctionEvaluations',10000);                     % Solving the optimisation problem
+[solution,fval,exitflag,output] = solve(prob,x0,'Options',options);                  % using inbuilt optimiser.
+C_new = solution.x;
+D_new = solution.y;
+
+function y = objective(C,D,B,S)                              % Objective function that has to be optimised. Eqn (10) in the paper.
+    y  = sum(dot(B,log(C))) - sum(dot(S,log(D)));
+end
+end
