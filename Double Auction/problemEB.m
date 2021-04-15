bc6108dd5070aff9d495648892634b58b893f05a
shreyasv11 <64632587+shreyasv11@users.noreply.github.com>
2021-04-15 16:02:31 +0530

    New code

--- Double Auction/problemEB.m
+++ Double Auction/problemEB.m
@@ -4,6 +4,6 @@ n = size(C,2);
 B = zeros(m,n);
 sum_C = sum(C,2)*neta;
 for i = 1:m
-    B(i,:) =  (neta*tau*C(i,:))/(STO(i)*(sum_C(i) - C_min(i) + 1));           % Problem EB. Eqn (13) in the paper.
+    B(i,:) =  (neta*tau*C(i,:))/(STO(i)*(sum_C(i) - C_min(i) + 1));          % Problem EB. Eqn (13) in the paper.
 end
 end
