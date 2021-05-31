04930bfd47ede3be1030cf8ca428eb60bf9f7546
shreyasv11 <64632587+shreyasv11@users.noreply.github.com>
2021-05-31 21:09:23 +0530

    New driver file

--- Double Auction/driver.m
+++ Double Auction/driver.m
@@ -25,8 +25,10 @@ STO = randi([1 5],m,1);      % Energy state before charging
 sw = zeros(18,1);
 %Charging Electricity
 ce = zeros(18,1);
+ce2 = zeros(18,1);
 %Discharging Electricity
 de = zeros(18,1);
+de2 = zeros(18,1);
 %Average payment
 ap = zeros(18,1);
 %Average reward
@@ -38,15 +40,22 @@ t = 1;
 while flag == 1
     [C, D] = problemA(B,S,neta,rho,C_min,C_max,D_max);           % Problem A solved to get C and D from initial B and S
     sw(t) = socialWelfare(C,D,neta,tau,STO,C_min,l1,l2);
-    ce(t) = sum(D(2,:));
+    ce(t) = sum(C(1,:));
     de(t) = sum(D(:,1));
+    ce2(t) = C(15,20);
+    de2(t) = D(20,15);
     %ap(t) = avgPayment(C, neta, tau, STO, C_min);
     ap(t) = avgPaymentNew(B);
     %ar(t) = avgReward(D, l1, l2);
     ar(t) = avgRewardNew(S, l1);
     B_prev = B;                                                  % Placeholder variable to store value of B
     S_prev = S;                                                  % Placeholder variable to store value of S
-    B = problemEB(C,neta,tau,C_min,STO);                         % Problem EB solved to get new B
+    
+    for i=1:m                                                    %This for loop solves EB by
+        B(i,:) = problemEBNew(C(i,:),neta,tau,C_min(i),STO(i));  %optimization. To use previous method,
+    end                                                          %comment this loop and uncomment next line
+    
+    %B = problemEB(C,neta,tau,C_min,STO);                         % Problem EB solved to get new B
     S = problemES(D,l1,l2);                                      % Problem ES solved to get new S
     RDB = mean2((abs(B - B_prev))./B);                           % RDB calculation for convergence check
     RDS = mean2((abs(S - S_prev))./S);                           % RDS calculation for convergence check
@@ -70,9 +79,20 @@ figure
 hold on
 plot(ce(1:end), '-b')
 plot(de(1:end), '--r')
+title('Using all PHEVs')
 legend('Charging Electricity', 'Discharging Electricity')
 xlabel('Iterations')
-ylabel('Amount if traded electricity')
+ylabel('Amount of traded electricity')
+hold off
+
+figure
+hold on
+plot(ce2(1:end), '-b')
+plot(de2(1:end), '--r')
+title('Using single PHEV')
+legend('Charging Electricity', 'Discharging Electricity')
+xlabel('Iterations')
+ylabel('Amount of traded electricity')
 hold off
 
 figure
