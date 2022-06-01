7ad1da3cd2375a1b853618de8ec59c1676cdf457
shreyasv11 <64632587+shreyasv11@users.noreply.github.com>
2022-06-01 13:24:50 +0530

    Add files via upload

---
+++ Heterogeneous Mixture/Buyers 30 Sellers 60/DAM/problemA.m
@@ -0,0 +1,28 @@
+function [C_new,D_new] = problemA(B,S,neta,rho,C_min,C_max,D_max)
+
+m = size(B,1);
+n = size(B,2);
+
+C = optimvar('x',m,n,'LowerBound',0);     % Electricity demand of charging vehicles
+D = optimvar('y',n,m,'LowerBound',0);     % Electricity supply by discharging vehicles
+
+prob = optimproblem;
+prob.Objective = fcn2optimexpr(@objective,C,D,B,S);    
+prob.Constraints.cons1 = (sum(C,2)*neta >= C_min);            % These are the constraints
+prob.Constraints.cons2 = (sum(C,2)*neta <= C_max);            % for the optimisation
+prob.Constraints.cons3 = (sum(D,2))<=D_max;          % problem. In the paper,
+prob.Constraints.cons4 = (rho*D == transpose(C));                         % they are written in eqn. (4).
+
+
+x0.x = ones(m,n);
+x0.y = ones(n,m);
+
+options = optimoptions(@fmincon,'MaxFunctionEvaluations',5000); % Solving the optimisation problem
+[solution,fval,exitflag,output] = solve(prob,x0,'Options',options);                  % using inbuilt optimiser.
+C_new = solution.x;
+D_new = solution.y;
+
+function y = objective(C,D,B,S)                              % Objective function that has to be optimised. Eqn (10) in the paper.
+    y = -(sum(sum(B.*log(C) - transpose(S.*D))));
+end
+end
